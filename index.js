81ff6b00bf6379a0255fcbc83353b0ad4b422ef8
Hristina Dokic <55371758+hdjokic@users.noreply.github.com>
2022-04-13 14:54:39 -0500

    node.js edited

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+//Database
+const Pool = require('pg').Pool
+const connectionParams = process.env.DATABASE_URL || {
+	user: 'api_user',
+	host: 'localhost',
+	database: 'api',
+	password: 'password',
+	port: 5432
+}
+
+const pool = new Pool({
+	user: 'api_user',
+	host: process.env.DATABASE_URL || 'localhost',
+	database: 'api',
+	password: 'password',
+	port: 5432
+	})
+	
+	
+	
 
 app.get('/', (req, res) => {
 	console.log('Accept: ' + req.get('Accept'))
-	res.send('<h1>Hello World!</h1>')
-	console.log('Content-type' + res.get('Content-Type'))
+	pool.query('SELECT VERSION()', (err, result)=>{
+		console.log(err, result.rows)
+		res.send('<h1>DB Version: ${result.rows[0].version} </h1>')
+		console.log('Content-type' + res.get('Content-Type'))
+	})
 })
 
 app.listen(port, () => {
