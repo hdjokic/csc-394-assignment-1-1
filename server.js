39742ac2bba133d8c14a7a47fb3632b7130ae661
Hristina Dokic <55371758+hdjokic@users.noreply.github.com>
2022-05-03 22:57:12 -0500

    finished testing

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+const {body, validationResult} = require('express-validator');
 
 //View Engine
 app.set("view engine", "pug")
 
+//parse xww-app
+app.use(bodyParser.urlencoded({extended:false}));
+
 //parse app
 app.use(bodyParser.json());
-
-//parse xww-app
-app.use(bodyParser.urlencoded({extended:true}));
 
 
 //Database
@@ -32,27 +33,45 @@ if (process.env.DATABASE_URL != null){
 		port: 5432
 	}
 }
-console.log(connectionParams)
+//console.log(connectionParams)
 const pool = new Pool(connectionParams)
 	
 	
 
 app.get('/', (req, res) => {
- console.log('Accept: ' + req.get('Accept'))
+ //console.log('Accept: ' + req.get('Accept'))
  pool.query('SELECT VERSION()', (err, version_results) => {
- console.log(err, version_results.rows)
+ //console.log(err, version_results.rows)
  pool.query('SELECT * FROM team_members', (err, team_members_results) => {
- console.log(err, team_members_results)
+ //console.log(err, team_members_results)
  res.render('index', {
- teamNumber: 3,
- databaseVersion: version_results.rows[0].version,
- teamMembers: team_members_results.rows
- })
- console.log('Content-Type: ' + res.get('Content-Type'))
+	teamNumber: 3,
+	databaseVersion: version_results.rows[0].version,
+	teamMembers: team_members_results.rows
+	})
+// console.log('Content-Type: ' + res.get('Content-Type'))
  })
  })
 })
+app.post('/',
+	body('first_name')
+		.isAlpha()
+		.isLength({min:1})
+		.withMessage('must be valid'),
+	body('last_name')
+		.isAlpha()
+		.isLength({min:0})
+		.withMessage('must be valid'),
+	(req,res) =>{
+		const errors = validationResult(req);
+		if(!errors.isEmpty()){
+			return res.status(400).send({errors:errors.array()});
+		}
 
+	pool.query('INSERT INTO team_members (first_name, last_name) VALUES ('${req.body.first_name}', '${req.body.last_name}')',(err, result) => {
+		res.redirect('/')
+	});	
+})
 
 
 module.exports = app;
