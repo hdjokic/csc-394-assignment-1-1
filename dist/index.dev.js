d5b44ef469f48f77a471ad80dee1a4ae24163703
Hristina Dokic <55371758+hdjokic@users.noreply.github.com>
2022-05-04 16:33:33 -0500

    edited

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -4,6 +4,6 @@ var app = require('./server');
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Example app listening on port ${port}');
+  console.log("Example app listening on port ${port}");
 });
 //# sourceMappingURL=index.dev.js.map
